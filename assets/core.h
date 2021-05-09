f75af0ec101e57898c7e5c9f47edb428512d70d3
Daneel Cayce Brown <dcbrown@liquidweb.com>
2021-05-08 20:16:47 -0400

    add Room class and convert existing rooms

--- assets/core.h
+++ assets/core.h
@@ -1,3 +1,8 @@
 ! -----------------------------------------------------------------------------
 ! Core header - mainly object classes right now so I don't have to repeat stuff too often.
 ! -----------------------------------------------------------------------------
+
+Class Room with
+    description
+        "Top-level room class. Helps distinguish between rooms and objects for readability, and cuts down on property statements.",
+has static light;
