3dd3a28dc602a643b0e0472d6545094668e00395
Daneel Cayce Brown <dcbrown@liquidweb.com>
2021-05-08 21:23:45 -0400

    create separate office story file, do some scene header stuff

--- engine/core.h
+++ engine/core.h
@@ -11,19 +11,33 @@ Class Room with
         "Top-level room class. Helps distinguish between rooms and objects for readability, and cuts down on property statements.",
 has static light;
 
+Class Scene with
+    initial [;
+        print "here is some initial scene text.";
+        Any();
+        self.description();
+    ]
+    description[;
+        "After the player moves to the scene, the dialogue plays out.";
+    ];
+has static light;
+
 
 ! "Press any key to continue"
+! Not clearing the screen right now, I don't think we need that
 [ Any x y;
     print "^^[Continue >]";
     @read_char 1 -> y;
-    ClearScreen();
+!    ClearScreen();
+    "^^"
 ];
 
 
-! Automatic scene playthrough - useful for cutscenes.
-[ PlayScene scene;
-  ClearScreen();
-  scene.initial();
-  move player to scene;
-  scene.description();
-];
+! Automatic scene playthrough - useful for cutscenes, maybe?
+! Also not clearing the screen, aesthetics can wait.
+![ PlayScene scene;
+!  ClearScreen();
+!  scene.initial();
+!  move player to scene;
+!  scene.description();
+!];
